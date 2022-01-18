a5c94a96a1a05d2989f99023d2a6af7cf3d75dd4
manu <rescue11080@gmail.com>
2022-01-18 14:49:01 +0530

    beginning to test...

--- source_mpi/scalar_f.cpp
+++ source_mpi/scalar_f.cpp
@@ -205,11 +205,12 @@ MPI_Status stdn,stup;
 	psi.mpi_send_recv();
 	
 	//printf("\ndk is %lf\n",dk);
-	
+	/*
 	if(use_omp)
 	fail = evolve_kdk_openmp(ind,n_axis_loc,psi,phi,k_grid,kbin_grid,a0,ai,a0,omega_dm_ini,dx,dk,kbins,0.4e-4,use_hdf5_format);
 	else
 	fail = evolve_kdk(ind,n_axis_loc,psi,phi,k_grid,kbin_grid,a0,ai,a0,omega_dm_ini,dx,dk,kbins,0.4e-4,use_hdf5_format);
+	*/
 	
 	printf("fail is %d\n",fail);
 	fftw_mpi_cleanup();
