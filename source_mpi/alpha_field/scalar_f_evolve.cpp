aa3795c946dabf18c8e1e820fbf7e45171bd7f5c
manu <rescue11080@gmail.com>
2022-02-05 13:29:16 +0530

    testing alpha..

--- source_mpi/alpha_field/scalar_f_evolve.cpp
+++ source_mpi/alpha_field/scalar_f_evolve.cpp
@@ -323,7 +323,7 @@ int evolve_kdk_openmp(int *n_glbl,int *n,field_alpha_mpi &f_alpha,metric_potenti
 	
 	
 
-	for(a=a_ini,a_print=a_ini,step_cnt=0;(a<=a0)&&(!fail)&&(step_cnt<1);t+=dt,++step_cnt)
+	for(a=a_ini,a_print=a_ini,step_cnt=0;(a<=a0)&&(!fail)&&(prn<=1);t+=dt,++step_cnt)
 	{
 	   //dt=dti*sqrt(a/a_ini);
 	 if(a>=a_print)
