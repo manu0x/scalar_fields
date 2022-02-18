7d25d2175d46ef4fc67b36fedc22167f6214da2d
manu <rescue11080@gmail.com>
2022-02-18 12:49:47 +0530

    started on FT based maybe implicit scheme..

---
+++ source_mpi/alpha_field2/scalar_f_back.cpp
@@ -0,0 +1,38 @@
+void set_back_cosmo(double &a0,double &ai,double &Hi,double &omega_dm_0,param_alpha p)
+{
+	double z = p.z_ini;
+	alpha = p.loc_alpha;
+	w = 1.0/(2.0*alpha-1.0);
+
+	c_box = p.loc_c_box;
+	pc_box = p.loc_pc_box;
+	hbar_box = p.loc_hbar_box;//eVs 
+	lenfac = p.loc_lenfac;
+	omega_dm_0 = p.omega_dm_0;
+	h = p.loc_h;
+	//hbar_by_m = hbar_box*c_box*(1e-8)/(alpha*pc_box);	
+	space_mpc_to_dimless = p.loc_space_mpc_to_dimless; ////	\tilde{x} (dimensionless) = physical{x (In Mpc)}*space_mpc_to_dimless  
+		
+	
+	
+	H0 = lenfac*(h/c_box)*0.001;
+	
+	
+	
+	a0 = p.a0;
+	ai = a0/(1.0+z);
+	Hi =   H0*sqrt(omega_dm_0*pow(a0/ai,3.0*(1.0+w))+ (1.0-omega_dm_0));
+	printf("H0 %lf Hi  %e\n",H0,Hi);
+
+	
+	p.print_to_file(fp_sim_info);
+	
+
+}
+
+
+double dlogD_dloga(double a)
+{
+	return (1.0);
+
+}
