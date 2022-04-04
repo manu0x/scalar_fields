60093c51091a84e53470082164eec68b146a19e2
manu <rescue11080@gmail.com>
2022-04-04 12:12:39 +0530

    compiles new changes..

--- include_mpi/alpha_field3/scalar_f_evolve.h
+++ include_mpi/alpha_field3/scalar_f_evolve.h
@@ -1,5 +1,5 @@
 /*int evolve_kdk(int *,int *,field_alpha_mpi &,metric_potential_approx_1_t_mpi &,double [][3],int [],double ,double,double,double,double,double *,double,int,double,int, bool);*/
-int evolve_kdk_openmp(int *,int *,metric_potential_poisson_mpi &,metric_potential_poisson_mpi &,double [][3],int [],double ,double,double,double,double,double *,double,int,double,int, bool);
+int evolve_kdk_openmp(int *,int *,metric_potential_poisson_mpi &,field_vel_mpi &,metric_potential_poisson_mpi &,double [][3],int [],double ,double,double,double,double,double *,double,int,double,int, bool);
 
 
 void evolve_hdf5_write(int *,metric_potential_poisson_mpi ,metric_potential_poisson_mpi ,hid_t ,double *,double ,double ,double ,double ,
--- include_mpi/alpha_field3/scalar_f_initial.h
+++ include_mpi/alpha_field3/scalar_f_initial.h
@@ -2,7 +2,7 @@
 double res_limits(double ,double ,double ,double ,double &,double & );
 void initial_hdf5_write_mpi(int *,int *,metric_potential_poisson_mpi ,metric_potential_poisson_mpi,hid_t ,double *,double [][3],double [][3],double ,double,int ,bool );
 
-void initialise_mpi(int * ,int *,metric_potential_poisson_mpi &,metric_potential_poisson_mpi &,metric_potential_poisson_mpi_ini &,
+void initialise_mpi(int * ,int *,metric_potential_poisson_mpi &,field_vel_mpi &,metric_potential_poisson_mpi &,metric_potential_poisson_mpi_ini &,
 				double ,int ,double ,double,double,double ,double &,double *,double &,int & ,
 								ini_power_generator ,gauss_rand_field_gen_mpi ,bool ,double,double,int,string);
 		   
