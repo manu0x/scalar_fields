0ebe1b7f25c5ff12a6b59d61ea2bfb67a16bcc0c
Manvendra Manu <rescue11080@gmail.com>
2023-06-16 17:23:15 +0300

    3d w expansion ready for testing

---
+++ GPE/GPE_classes_3d_exp_uni.cpp
@@ -0,0 +1,695 @@
+
+
+///#include <omp.h>
+#include <fftw3.h>
+#include <math.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <algorithm>
+#include <hdf5.h>
+
+
+
+class GPE_field_3d
+{
+    private:
+    
+    double hbar_unit,c_unit,h,pc_unit;
+    double vfac;
+    
+    double m_alpha;
+
+    public:
+    double kppa,omega_m0,ai;
+
+    fftw_complex *psi;
+
+    fftw_complex *fpGpsi;
+	fftw_complex *fpGpsi_ft;
+
+    fftw_complex *V_phi;
+	fftw_complex *V_phi_ft;
+
+	fftw_complex *K;
+	fftw_complex *K_ft;
+
+   
+
+    double ***im_K_psi = new double**[2];
+	double ***ex_K_psi = new double**[2];
+
+	fftw_plan plan_pois_f;
+	fftw_plan plan_pois_b;
+	fftw_plan plan_imp_b;
+
+    fftw_plan plan_V_f;
+    fftw_plan plan_V_b;
+
+
+
+    int j,N,N2,N3,s;
+    int i,k;
+
+    double dj,dN3;
+    double energy,ini_energy,max_eng_err;
+    double mass,ini_mass,max_mass_err;
+
+
+
+    GPE_field_3d(int jj,int NN,int imex_s,int nthreads=4)
+    {
+        j= jj;
+        N= NN;
+        N2 = N*N;
+        N3 = N*N*N;
+        s = imex_s;
+        dj = (double)j;
+        dN3 = (double)N3;
+
+        fpGpsi = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * (N3));
+	    fpGpsi_ft =(fftw_complex*) fftw_malloc(sizeof(fftw_complex) *(N3));
+
+        psi = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * (N3));
+	    
+
+	    K = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * (N3));
+	    K_ft =(fftw_complex*) fftw_malloc(sizeof(fftw_complex) *(N3));
+
+        V_phi = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * (N3));
+	    V_phi_ft =(fftw_complex*) fftw_malloc(sizeof(fftw_complex) *(N3));
+
+        fftw_plan_with_nthreads(nthreads);
+
+
+	    plan_pois_f = fftw_plan_dft_3d(N,N,N, fpGpsi, fpGpsi_ft, FFTW_FORWARD, FFTW_ESTIMATE);
+	    plan_pois_b = fftw_plan_dft_3d(N,N,N, fpGpsi_ft, fpGpsi, FFTW_BACKWARD, FFTW_ESTIMATE);
+
+        plan_V_f = fftw_plan_dft_3d(N,N,N, V_phi, V_phi_ft, FFTW_FORWARD, FFTW_ESTIMATE);
+	    plan_V_b = fftw_plan_dft_3d(N,N,N, V_phi_ft, V_phi, FFTW_BACKWARD, FFTW_ESTIMATE);
+
+
+	    plan_imp_b = fftw_plan_dft_3d(N,N,N, K_ft, K, FFTW_BACKWARD, FFTW_ESTIMATE);
+        
+
+        for(i=0;i<2;++i)
+	    {
+		    im_K_psi[i] = new double*[imex_s];
+		    ex_K_psi[i] = new double*[imex_s];
+
+		    for(k=0;k<imex_s;++k)
+		    {im_K_psi[i][k] =  new double[N3];
+		     ex_K_psi[i][k] =  new double[N3];
+		    }
+
+
+
+
+
+	    }
+
+
+    }
+
+    double HbyH0(double a,double a0=1.0)
+    {
+
+        return( sqrt(omega_m0*(a0/a)*(a0/a)*(a0/a) + (1.0 - omega_m0))    );
+
+
+    }
+
+
+    void solve_V(double *kgrid)
+    {
+        double psisqr,ksqr;  int loc_i,loc_j,loc_k,ii;
+
+        #pragma omp parallel for private(psisqr)
+        for(loc_i=0;loc_i<N3;++loc_i)
+        {
+            psisqr = fpGpsi[loc_i][0]*fpGpsi[loc_i][0] + fpGpsi[loc_i][1]*fpGpsi[loc_i][1];
+            V_phi[loc_i][0] = (0.5/kppa)*(psisqr-3.0*omega_m0);
+            V_phi[loc_i][1] = 0.0;
+
+
+        }
+        fftw_execute(plan_V_f);
+
+        for(ii=0,loc_i=-1,loc_j=-1,loc_k=0;ii<N3;++ii,++loc_k)
+        {
+            if(ii%N ==0)
+            {
+                loc_k=0;
+                ++loc_j;
+                if(ii%N2==0)
+                {
+                    loc_j=0;
+                    ++loc_i;
+
+
+                }
+
+
+            }
+            ksqr = kgrid[loc_i]*kgrid[loc_i] + kgrid[loc_j]*kgrid[loc_j] + kgrid[loc_k]*kgrid[loc_k];
+
+          if(ksqr>0.0)
+           { V_phi_ft[ii][0] = -V_phi_ft[ii][0]/(ksqr*dN3);
+                
+             V_phi_ft[ii][1] = -V_phi_ft[ii][1]/(ksqr*dN3);
+           }
+            else
+          {
+            V_phi_ft[ii][0] = 0.0;
+            V_phi_ft[ii][1] = 0.0;
+
+          }
+
+
+        }
+
+        fftw_execute(plan_V_b);
+
+
+        
+
+
+    }
+   
+    void update_fft_fields(int ind,double ksqr,double lamda)
+    {
+       
+       
+       	fpGpsi_ft[ind][0] = ((fpGpsi_ft[ind][0]) + lamda*fpGpsi_ft[ind][1])/(1.0+lamda*lamda);
+		fpGpsi_ft[ind][1] = ((fpGpsi_ft[ind][1]) - lamda*fpGpsi_ft[ind][0])/(1.0+lamda*lamda);
+        
+
+
+		fpGpsi_ft[ind][0] = fpGpsi_ft[ind][0]/((double)N3);
+		fpGpsi_ft[ind][1] = fpGpsi_ft[ind][1]/((double)N3);
+
+       
+		K_ft[ind][0] = -fpGpsi_ft[ind][0]*ksqr;
+		K_ft[ind][1] = -fpGpsi_ft[ind][1]*ksqr;
+       
+       
+
+
+
+
+    }
+
+    void ex_rhs(int ind,int stg_i,double expnfac=1.0)
+    {
+            
+
+            ex_K_psi[0][stg_i][ind] =  V_phi[ind][0]*fpGpsi[ind][1]/expnfac;
+            ex_K_psi[1][stg_i][ind] = -V_phi[ind][0]*fpGpsi[ind][0]/expnfac;
+            
+
+            
+
+
+
+    }
+
+    void im_rhs(int ind,int stg_i,double expnfac=1.0)
+    {
+        im_K_psi[0][stg_i][ind]  = -K[ind][1]*0.5*kppa/expnfac; 
+        im_K_psi[1][stg_i][ind]=  K[ind][0]*0.5*kppa/expnfac;
+
+
+    }
+
+    void do_forward_fft()
+    {
+        fftw_execute(plan_pois_f);
+       // fftw_execute(plan_V_f);
+
+
+    }
+
+    void do_back_fft()
+    {
+        fftw_execute(plan_pois_b);
+		fftw_execute(plan_imp_b);
+       // fftw_execute(plan_V_b);
+
+    }
+
+    void read_from_file(char  *fname)
+    {
+        FILE *fp_param = fopen(fname,"r");
+
+        int j,i=0;
+        size_t fnd_div;
+        double num_val;
+        int max_len = 80;
+	    char read_c_line[max_len];
+	    string read_str,cur_str, read_key, read_val,cur_num,cur_den;
+
+        while(fgets(read_c_line,max_len,fp_param)!=NULL)
+        {  read_str = read_c_line; //printf("re str  %s  %d\n",read_str.c_str(),read_str.length());
+            read_key = read_str.substr(0,read_str.find("="));
+		  remove_if(read_key.begin(),read_key.end(),::isspace);
+		  read_key.erase(remove_if(read_key.begin(),read_key.end(),::isspace),read_key.end());
+		 
+
+		 if(read_key.length()>0)
+		 {  read_val = read_str.substr(read_str.find("=")+1);
+		   
+
+		   
+		
+		   if(read_key=="c_unit")
+			{
+                num_val = stod(read_val);
+                c_unit = num_val;
+
+            }
+		   else if (read_key=="hbar_unit")
+           {
+               num_val = stod(read_val);
+               hbar_unit = num_val;
+           }
+           else if (read_key=="pc_unit")
+           {
+               num_val = stod(read_val);
+               pc_unit = num_val;
+           }
+           else if (read_key=="h")
+           {
+               num_val = stod(read_val);
+               h = num_val;
+           }
+           else if (read_key=="m_alpha")
+           {
+               num_val = stod(read_val);
+               m_alpha = num_val;
+           }
+           
+           else if (read_key=="omega_m0")
+           {
+               num_val = stod(read_val);
+               omega_m0= num_val;
+           }
+		   else if (read_key=="ai")
+           {
+               num_val = stod(read_val);
+               ai= num_val;
+           }
+
+
+         }
+
+    
+
+
+
+        }
+    }
+
+    void print_params_set_kappa()
+    {
+
+        printf("\nPrinting params for field %d\n",j+1);
+        printf("[c_unit,hbar_unit,pc_unit] = [%lf,%lf,%lf]\n",c_unit,hbar_unit,pc_unit);
+        printf("[h,omega_m0,m_alpha] = [%lf,%lf,%lf] and ai = %lf \n",h,omega_m0,m_alpha,ai);
+        kppa = c_unit*c_unit*hbar_unit*h*(1e-5)/(m_alpha*pc_unit);
+
+        printf("kappa = %lf\n\n",kppa);
+    }
+
+    void initialise_random(double *kgrid,double amp=1e-1)
+    {
+        int ii,loc_i,loc_j,loc_k;
+        double uni_rand,theta_rnd;
+        double psiamp,ksqr,loc_delta;
+        double vmax;
+
+        double rmx = (double)RAND_MAX;
+        for(ii=0;ii<N3;++ii)
+        {
+
+            uni_rand = amp*( -1.0 + 2.0*((double)rand())/rmx);
+
+            
+
+            fpGpsi[ii][0] = uni_rand;
+            fpGpsi[ii][1] = 0.0;
+
+
+        }
+
+        fftw_execute(plan_pois_f);
+        fpGpsi_ft[0][0] = 0.0;   fpGpsi_ft[0][1] = 0.0;
+        fftw_execute(plan_pois_b);
+        double dn3 = (double)N3;
+        double dsum = 0.0;
+        for ( ii = 0,loc_i=-1,loc_j=-1; ii < N3; ++ii,++loc_k)
+        {   
+            if(ii%N ==0)
+            {
+                loc_k=0;
+                ++loc_j;
+                if(ii%N2==0)
+                {
+                    loc_j=0;
+                    ++loc_i;
+
+
+                }
+
+
+            }
+            ksqr = kgrid[loc_i]*kgrid[loc_i] + kgrid[loc_j]*kgrid[loc_j] + kgrid[loc_k]*kgrid[loc_k];
+
+           loc_delta = fpGpsi[ii][0]/dn3;
+           dsum+=loc_delta;
+
+           psiamp = sqrt(3.0*omega_m0*(1.0+loc_delta));
+           theta_rnd = 0.000*(((double)rand())/rmx)*2.0*M_PI;
+           psi[ii][0] = psiamp*cos(theta_rnd);
+           psi[ii][1] = psiamp*sin(theta_rnd);
+
+           V_phi_ft[ii][0] = 1.5*omega_m0*fpGpsi_ft[ii][0]/(dn3*kppa*ksqr);
+           V_phi_ft[ii][1] = 1.5*omega_m0*fpGpsi_ft[ii][1]/(dn3*kppa*ksqr);
+
+           if(isnan(psi[ii][0]+psi[ii][1]))
+           {
+            printf("ini break %d %lf %lf\n",ii,psi[ii][0],psi[ii][1]);
+            break;
+
+           }
+           
+        }
+        V_phi_ft[0][0] = 0.0; V_phi_ft[0][1] = 0.0;
+
+        fftw_execute(plan_V_b);
+
+        vmax = -10000000000.0;
+        printf("dsum delta is %.10lf\n",dsum);
+        dsum = 0.0;
+        for ( ii = 0; ii < N3; ++ii,++loc_k)
+        { 
+
+            if(fabs(V_phi[ii][0])>vmax)
+                vmax = V_phi[ii][0];
+
+                dsum+= (psi[ii][0]*psi[ii][0]+psi[ii][1]*psi[ii][1])/3.0 -1.0;
+
+              
+
+        }
+
+
+        printf("Initial random done with vmax %.10lf and dsum is %.7lf\n",vmax,dsum);
+
+
+
+    }
+    
+
+    void set_field()
+    {int ii;
+        for(ii=0;ii<N3;++ii)
+        {
+
+            fpGpsi[ii][0] = psi[ii][0];
+            fpGpsi[ii][1] = psi[ii][1];
+
+        }
+
+        fftw_execute(plan_pois_f);
+
+        for(ii=0;ii<N3;++ii)
+        {
+
+            fpGpsi_ft[ii][0] = fpGpsi_ft[ii][0]/(dN3);
+            fpGpsi_ft[ii][1] = fpGpsi_ft[ii][1]/(dN3);
+
+        }
+
+        fftw_execute(plan_pois_b);
+
+        
+
+    }
+
+    void reset_consv_quant(int is_ini=0)
+    {
+        energy = 0.0;
+        mass = 0.0;
+        if(is_ini)
+        {
+            ini_energy=0.0;
+            ini_mass = 0,0;
+
+            max_eng_err=-1.0;
+            max_mass_err=-1.0;
+
+        }
+
+    }
+    void cal_conserve_at_point(int ci,double dx,int is_ini=0)
+    {
+           
+        
+            double loc_energy,loc_mass,psi2;//der_x[2],der_y[2],der_amp2,fcntr,Rcntr;
+         /*     int left[2],right[2],left_y,right_y,ci,cr,cl;   
+           
+            /////////////////////  x-derivative //////////////////////
+          left[0] = ind[0]-1;
+            left[1] = ind[1];
+            if(ind[0]==0)
+            left[0] = N-2;
+
+            right[0] = ind[0]+1;
+            right[1] = ind[1];
+            if(ind[0]==(N-1))
+            right[0] = 1;
+
+            cr = right[0]*N+right[1];
+            cl = left[0]*N+left[1];
+
+            der_x[0] = (psi[cr][0]+psi[cl][0]-2.0*psi[ci][0])/dx;
+            der_x[1] = (psi[cr][1]+psi[cl][1]-2.0*psi[ci][1])/dx;
+
+            ///////////////////////////////////
+
+            /////////////////////  y-derivative //////////////////////
+            left[0] = ind[0];
+            left[1] = ind[1]-1;
+            if(ind[1]==0)
+            left[1] = N-2;
+
+            right[0] = ind[0];
+            right[1] = ind[1]+1;
+            if(ind[1]==(N-1))
+            right[1] = 1;
+
+            cr = right[0]*N+right[1];
+            cl = left[0]*N+left[1];
+
+            der_y[0] = (psi[cr][0]+psi[cl][0]-2.0*psi[ci][0])/dx;
+            der_y[1] = (psi[cr][1]+psi[cl][1]-2.0*psi[ci][1])/dx;
+
+            ///////////////////////////////////  (der_x)^2+(der_y)^2    /////////////////
+            der_amp2 =    der_x[0]*der_x[0] + der_x[1]*der_x[1] + der_y[0]*der_y[0] + der_y[1]*der_y[1];
+         */   ///////////////////////////////////////////////////
+            psi2 = psi[ci][0]*psi[ci][0] + psi[ci][1]*psi[ci][1];
+
+         //   fcntr = 0.5*bta[0]*psi2*psi2 + 0.5*bta[1]*psi2*psi2_other;
+           // Rcntr = -(psi[ci][0]*     )
+      
+
+           loc_energy = 0.0;// 0.5*der_amp2 +  V(x)*psi2 + fcntr + Rcntr;
+            loc_mass = psi2;
+
+            energy+=loc_energy;
+            mass+=loc_mass;
+
+            if(is_ini)
+            {
+                ini_energy+=loc_energy;
+                ini_mass+=loc_mass;
+
+            }
+
+            
+    }
+
+    void conserve_err()
+    {
+        double eng_err,mass_err;
+        eng_err = fabs(energy-ini_energy)/fabs(ini_energy);
+        mass_err = fabs(mass-ini_mass)/ini_mass;
+
+        if(eng_err>max_eng_err)
+            max_eng_err=eng_err;
+        
+        if(mass_err>max_mass_err)
+            max_mass_err=mass_err;
+
+
+    }
+
+
+
+
+void read_from_initial()
+{
+    double a[10],fr,fi;
+
+    FILE *fp = fopen("initial1.txt","r");
+    for(i=0;i<N3;++i)
+    {
+
+        fscanf(fp,"%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n",&a[0],&a[1],&a[2],&a[3],&fr,&fi,&a[4],&a[5],&a[6],&a[7],&a[8],&a[9]);
+        psi[i][0] = fr/100.0;//H0=100 in given units and it is a unit conversion
+        psi[i][1] = fi/100.0;
+
+    }
+
+
+
+}
+
+void read_dc_from_hdf5(string fname,double *dc,double *theta)
+{
+
+	herr_t status;	
+	hid_t file,dtype,dspace_glbl,dspace,dset_glbl,plist_id,g_id;
+	
+
+	hsize_t dim[3],odim[2];
+
+	dim[0] = N;
+	dim[1] = N;
+	dim[2] = N;
+	odim[0] = N3;
+	odim[1] = 1;
+
+	plist_id = H5Pcreate(H5P_FILE_ACCESS);
+       
+
+	
+	char fchar_name[fname.length()];
+	fname.copy(fchar_name,fname.length()-1,0);
+	printf("FINI_dc %s %d\n",fchar_name,fname.length());
+	
+	file = H5Fopen(fchar_name, H5F_ACC_RDONLY, plist_id);
+	H5Pclose(plist_id);
+
+	
+	dtype = H5Tcopy(H5T_NATIVE_DOUBLE);
+    	status = H5Tset_order(dtype, H5T_ORDER_LE);
+
+
+	//dspace_glbl = H5Screate_simple(2, odim, NULL);
+
+	//g_id = H5G.open(file, "/");
+
+	dset_glbl = H5Dopen(file, "/dc",H5P_DEFAULT);
+
+	dspace_glbl = H5Dget_space(dset_glbl);
+
+	hsize_t count[2],offset[2];
+	count[0] = N3; count[1] = odim[1]; offset[0] = 0; offset[1] = 0;
+
+	
+
+	dspace = H5Screate_simple(2, count, NULL);
+
+	
+
+	
+
+	status = H5Dread(dset_glbl, dtype, dspace, dspace_glbl, plist_id, dc);
+
+	H5Dclose(dset_glbl);
+	H5Pclose(plist_id);
+	H5Sclose(dspace);
+
+
+	dset_glbl = H5Dopen(file, "/theta",H5P_DEFAULT);
+
+	dspace_glbl = H5Dget_space(dset_glbl);
+
+
+	count[0] = N3; count[1] = 1; 
+
+	
+
+	dspace = H5Screate_simple(2, count, NULL);
+
+	
+
+	
+
+	
+
+	status = H5Dread(dset_glbl, dtype, dspace, dspace_glbl, plist_id, theta);
+
+	H5Dclose(dset_glbl);
+	H5Pclose(plist_id);
+	H5Sclose(dspace);
+
+
+
+
+
+
+
+}
+
+
+   
+
+};
+
+
+
+
+
+
+
+    
+
+/*
+int main(int argc,char **argv)
+{
+
+
+	char fp_name[30]("imex_ft_");
+	
+    int stages;
+	char *imex_file; 
+    char *f1file;
+
+    stages = atoi(argv[1]);
+	imex_file = argv[2];
+    f1file = argv[3];
+	
+	strcat(fp_name,imex_file);
+	printf("ImEx table filename is %s and stages given by u is %d and out file %s \n",imex_file,stages,fp_name);
+
+	FILE *fp = fopen(fp_name,"w");
+	
+
+	imex_table imx(stages);
+    
+ 
+  	imx.read_from_file(imex_file);
+  	imx.print_table();
+
+
+    GPE_field_2d  psi_1(0,128,3,8);
+    GPE_field_2d  psi_2(1,128,3,8);
+
+    psi_1.read_from_file(f1file);
+
+    psi_1.print_params();
+
+}*/
---
+++ mock_fdm/fdm_imex_3d_in_a.cpp
@@ -0,0 +1,544 @@
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+
+#include <fftw3.h>
+
+
+#include <limits>
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+
+#include <algorithm>
+#include "../imex/imex_classes.cpp"
+#include "../GPE/GPE_classes_3d_exp_uni.cpp"
+
+using namespace std;
+
+#define pie M_PI
+
+
+
+//////////GLobal constants/////////////
+
+
+
+///////////////////////////////////////
+
+
+
+
+
+
+
+
+void initialise_kgrid(double *k,double dx,int N)
+{
+
+	int i,j,l,ci; double di,dj,dl,dci,dk;
+	dk = 2.0*pie/(((double)(N))*dx);
+	
+	for(i=0;i<N;++i)
+	{
+	   di = (double)i;
+
+	   if(i<(N/2))
+		*(k+i) = di*dk;
+	    else
+		*(k+i) = (di-((double) N))*dk;
+
+
+	}
+
+
+
+
+
+
+}
+
+
+double run(double da,int N,double *mass_err,int argc,char **argv,int prntfp,int prnt)
+{
+
+	int a_steps;
+	double box_len,a,a_end,a_start,ak,xval,dbi,dbj,dbk,dx;
+	double HbyH0;
+	double x0[3],xv[3];
+
+	fftw_init_threads();
+	
+///////////////////////////////File for data/////////////////////////////////////////////////////
+/*
+	FILE *fp = fopen("data_ft.txt","w");
+	FILE *fp2 = fopen("data2_ft.txt","w");
+	FILE *fplap = fopen("lap_ft.txt","w");
+	FILE *fpmass = fopen("mass_ft.txt","w");
+	FILE *fptime = fopen("tm_ft.txt","w");
+*/
+/////////////////////////////// Parameter setting ////////////////////////////////////////////////
+	
+
+////////////	Imex declare and data write file declare ////////////////////////
+
+
+
+	char fp_name[30]("data_");
+
+
+    int stages;
+	char *imex_file; 
+   
+
+
+    stages = atoi(argv[1]);
+	imex_file = argv[2];
+    
+	
+	
+	strcat(fp_name,imex_file);
+	printf("ImEx table filename is %s and stages given by u is %d and out file %s \n",imex_file,stages,fp_name);
+
+	FILE *fp = fopen(fp_name,"w");
+	
+	
+	imex_table imx(stages);
+    
+	
+  	imx.read_from_file(imex_file);
+  	imx.print_table();
+   
+	
+
+////////////////////////	Declare field /////////////////////////////////////////////
+char *f1paramfile;
+
+GPE_field_3d  psi_1(0,N,imx.s,16);
+
+f1paramfile = argv[3];
+
+psi_1.read_from_file(f1paramfile);
+	
+
+psi_1.print_params_set_kappa();
+
+
+
+
+
+
+/////////////////////////////// Box & res. setting ///////////////////////////////////////////////
+	box_len = 1.0;
+	x0[0]=-0.5*box_len; x0[1]=-0.5*box_len; x0[2]=-0.5*box_len;
+	
+	//n  = 2.0/box_len;
+	N = 128;
+	dx = box_len/(double(N));
+	
+	//N = ((int)(box_len/dx));
+	int N3 = N*N*N;
+	int N2 = N*N;
+	printf("N2 %d\n",N2);
+////////////////////////////// Time & da settings ////////////////////////////////////////////////
+
+	a_start = psi_1.ai;
+	a_end = 1.0;
+	
+	a_steps = (int)((a_end-a_start)/da);
+	
+	if(prnt)
+	printf("da %lf N %d\n",da,N);
+
+
+	
+/////////////////////////////////////////RK things/////////////////////////////////////////
+	int i,j;
+
+////////////////////////////// k things /////////////
+
+	double k_grid[N],ksqr;
+
+
+//////////////////////////////  variables  /////////////////////////////////////////////////////
+
+
+
+	double lambda;
+
+
+
+    
+	
+
+	initialise_kgrid(k_grid,dx,N);
+
+	//psi_1.initialise_random(k_grid) ; 
+	psi_1.read_from_initial();
+	
+	
+	
+	//printf("no of threads %d\n",omp_get_num_threads());
+	//omp_set_num_threads(4);
+	//printf("no of threads %d\n",omp_get_num_threads());
+	
+
+	if(prnt)
+	printf("Initialization done\n");
+	
+	psi_1.set_field();
+	
+
+
+///////////////////////  Evolution ///////////////////////////////////////////////////////////////
+    int ii,jj,kk;
+	int s_cntr,acntr,printcntr,fpcntr,err_cntr=1,fail=0;
+
+	printcntr = (int)(((double) a_steps)/100.0);
+	fpcntr = (int)(((double) a_steps)/10.0);
+	if(a_steps<=100)
+	{	printcntr = 1.0;
+	
+	}	//printf("%d\n",printcntr);
+	double vel_val[2],c_psi[2],c_psi_amp2,delta,amp,avg_amp;
+	
+	double amp_ini,dsum,fac,vmax=-0.00000000000001;
+	
+
+	double kv[3];
+	int ind[3];
+
+
+	ii=-1;
+	jj=-1;
+	kk=0;
+
+	psi_1.reset_consv_quant(1);
+	psi_1.solve_V(k_grid);
+	HbyH0 = psi_1.HbyH0(a_start);
+	fac = a_start*a_start*HbyH0;
+	for ( i = 0; i < N3; i++)
+	{
+		if(vmax<fabs(psi_1.V_phi[i][0]/fac))
+					vmax = fabs(psi_1.V_phi[i][0]/fac);
+	}
+	
+
+	if(vmax>(imx.ex_stb_r/da))
+			printf("start vmax da %lf stb r %lf\n",vmax*da,imx.ex_stb_r);
+	
+	printf("Starting Run..,\n");
+
+	
+	for(a=a_start,acntr=0;(a<=a_end)&&(!fail)&&(30);a+=da,++acntr)
+	{
+
+		
+		if((acntr%printcntr==0)&&prnt)
+		printf("time %lf %lf %lf  net mass %lf\n",a/a_end,psi_1.mass,psi_1.mass,vmax);
+		psi_1.do_forward_fft();
+		
+
+		psi_1.reset_consv_quant();
+		dsum = 0.0;
+		
+		
+	 	for(i=0,ii=-1,jj=-1,kk=0;i<(N3);++i,++kk)
+		{	
+
+			
+
+			if(i%N ==0)
+            {
+                kk=0;
+                ++jj;
+                if(i%N2==0)
+                {
+                    jj=0;
+                    ++ii;
+
+
+                }
+			}
+			
+
+			dbi = (double)(ii); dbj = (double)(jj); dbk = (double)(kk);
+			xv[0] = x0[0] + dx*dbi; xv[1] = x0[1] + dx*dbj; xv[2] = x0[2] + dx*dbk;
+			if(acntr%err_cntr==0)
+			{
+				psi_1.cal_conserve_at_point(i, dx,!acntr);
+				
+
+			}
+
+			if((acntr%fpcntr==0)&&(prntfp))
+			{
+				
+				c_psi_amp2 = (psi_1.psi[i][0]*psi_1.psi[i][0]+psi_1.psi[i][1]*psi_1.psi[i][1]);
+				delta = (c_psi_amp2/(3.0*psi_1.omega_m0) -1.0);
+				dsum+=delta; 
+				fprintf(fp,"%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n",xv[0],xv[1],xv[2],psi_1.psi[i][0],psi_1.psi[i][1],c_psi_amp2,delta);
+
+
+			}
+			ak = a+imx.im_c[0]*da;
+			HbyH0 = psi_1.HbyH0(ak);
+			ksqr = k_grid[ii]*k_grid[ii] + k_grid[jj]*k_grid[jj] + k_grid[kk]*k_grid[kk];
+			lambda = ksqr*imx.im_a[0][0]*psi_1.kppa*da/(ak*ak*ak*HbyH0*2.0);
+			//printf("lmda  %lf\n",lambda);
+			
+			
+
+			psi_1.update_fft_fields(i,ksqr,lambda);
+			
+
+		}
+		if((acntr%fpcntr==0)&&(prntfp))
+			printf("a/a_end %lf dsum = %lf\n",a/a_end,dsum);
+		if(acntr%err_cntr==0)
+		{	psi_1.conserve_err();
+			
+
+		}
+
+		if((acntr%fpcntr==0)&&(prntfp))
+		fprintf(fp,"\n\n\n");
+		
+		
+		psi_1.do_back_fft();
+		psi_1.solve_V(k_grid);
+		
+
+		for(s_cntr=1;s_cntr<imx.s;++s_cntr)
+		{
+			
+			for(j=0;j<s_cntr;++j)
+			{
+			   for(i=0;i<N3;++i)
+			   {
+				
+ 				if(j==0)
+				{
+				
+					ak = a+imx.ex_c[s_cntr-1]*da;
+					HbyH0 = psi_1.HbyH0(ak);
+					fac = ak*ak*HbyH0;
+
+					if(vmax<fabs(psi_1.V_phi[i][0]/fac))
+					vmax = fabs(psi_1.V_phi[i][0]/fac);
+
+					psi_1.ex_rhs(i,s_cntr-1,fac);
+					
+					
+					
+			
+					ak = a+imx.ex_c[s_cntr-1]*da;
+					HbyH0 = psi_1.HbyH0(ak);
+					psi_1.im_rhs(i,s_cntr-1,ak*ak*ak*HbyH0);
+
+					
+					
+
+					
+					
+
+					psi_1.fpGpsi[i][0] = psi_1.psi[i][0] + da*imx.ex_a[s_cntr][j]*psi_1.ex_K_psi[0][j][i]+ da*imx.im_a[s_cntr][j]*psi_1.im_K_psi[0][j][i];
+					
+
+					
+
+				}
+
+
+				else
+				{psi_1.fpGpsi[i][0]+=  da*imx.ex_a[s_cntr][j]*psi_1.ex_K_psi[0][j][i]+ da*imx.im_a[s_cntr][j]*psi_1.im_K_psi[0][j][i];
+				 psi_1.fpGpsi[i][1]+=  da*imx.ex_a[s_cntr][j]*psi_1.ex_K_psi[1][j][i]+ da*imx.im_a[s_cntr][j]*psi_1.im_K_psi[1][j][i];
+				}
+
+
+
+			   }
+
+
+			}
+
+			if(vmax>(imx.ex_stb_r/da))
+			printf("acntr %d  vmax da %lf stb r %lf\n",acntr,vmax*da,imx.ex_stb_r);
+
+			psi_1.do_forward_fft();
+			
+		
+
+
+			for(i=0,ii=-1,jj=-1,kk=0;i<N3;++i,++kk)
+			{
+			
+				if(i%N ==0)
+            	{
+                	kk=0;
+                	++jj;
+                	if(i%N2==0)
+                	{
+                    	jj=0;
+                    	++ii;
+
+
+                	}
+				}
+				
+				
+				ak = a+imx.im_c[s_cntr]*da;
+				HbyH0 = psi_1.HbyH0(ak);
+				ksqr = k_grid[ii]*k_grid[ii] + k_grid[jj]*k_grid[jj] + k_grid[kk]*k_grid[kk];
+				
+			 	lambda = ksqr*imx.im_a[s_cntr][s_cntr]*psi_1.kppa*da/(ak*ak*ak*HbyH0*2.0);
+			 	
+
+			 	psi_1.update_fft_fields(i,ksqr,lambda);
+			 
+
+
+
+			}
+
+			
+
+
+			psi_1.do_back_fft();
+			psi_1.solve_V(k_grid);
+
+
+
+		}//RK stages concluded
+
+
+
+		
+
+
+		for(i=0,ii=-1,jj=-1,kk=0;i<N3;++i,++kk)
+		{
+			
+				
+
+
+			
+			
+			ak = a+imx.ex_c[imx.s-1]*da;
+			HbyH0 = psi_1.HbyH0(ak);
+			psi_1.ex_rhs(i,imx.s-1,ak*ak*HbyH0);
+
+			
+			ak = a+imx.im_c[imx.s-1]*da;
+			HbyH0 = psi_1.HbyH0(ak);
+			psi_1.im_rhs(i,imx.s-1,ak*ak*ak*HbyH0);
+		
+
+
+			for(j=0;j<imx.s;++j)
+			{	psi_1.psi[i][0]+=  da*imx.ex_b[j]*psi_1.ex_K_psi[0][j][i]+ da*imx.im_b[j]*psi_1.im_K_psi[0][j][i];
+				psi_1.psi[i][1]+=  da*imx.ex_b[j]*psi_1.ex_K_psi[1][j][i]+ da*imx.im_b[j]*psi_1.im_K_psi[1][j][i];
+
+				
+
+
+
+
+			}
+
+			psi_1.fpGpsi[i][0] = psi_1.psi[i][0];
+			psi_1.fpGpsi[i][1] = psi_1.psi[i][1];
+
+			
+
+
+			if(isnan((psi_1.psi[i][0]+psi_1.psi[i][1])+(psi_1.V_phi[i][0])))
+			{
+				fail =1;
+				//if(prt)
+				printf("FAILED %d acntr %d %lf %lf %lf %lf\n",i,acntr,psi_1.psi[i][0],psi_1.psi[i][1],psi_1.V_phi[i][0],psi_1.V_phi[i][1]);
+
+				break;
+
+			}
+
+
+
+		   
+		}
+
+		
+
+
+	}///// ENd f Time Evolution /////////////////////////
+
+	fclose(fp);
+	fftw_cleanup_threads();
+	*mass_err = psi_1.max_mass_err;
+	*(mass_err+1)= psi_1.max_mass_err;
+
+
+	if(prnt)
+	printf("N %d\n Run en los %lf abs err %lf\n",N,*(mass_err),*(mass_err+1));
+
+
+	
+	return(psi_1.max_mass_err);
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+int main(int argc, char ** argv)
+{
+
+	double da = 3e-4;
+	double dx = 4e-3;
+	double mass_err[2],mass_loss;
+
+	double dx_l=2e-3,dx_u = 4e-2;
+	double da_l= 1e-5,da_u = 1e-2;
+
+	double ddx = (dx_u-dx_l)/(20.0);
+	double dda = (da_u-da_l)/(20.0);
+
+	FILE *fp = fopen("imex_ft.txt","w");
+	
+
+	//for(da=da_l;da<=da_u;da+=dda)
+	{
+
+
+		//for(dx = dx_l;dx<=dx_u;dx+=ddx)
+		{
+			dx = 2e-2;
+			da = 1e-4;
+			mass_loss = run(da,512,mass_err,argc,argv,1,1);
+
+			printf("%lf\t%lf\t%lf\t%lf\t%lf\t%.10lf\n",dx,da,da/(dx*dx),mass_loss,*mass_err,*(mass_err+1));
+			fprintf(fp,"%lf\t%lf\t%lf\t%lf\t%lf\t%.10lf\n",dx,da,da/(dx*dx),mass_loss,*mass_err,*(mass_err+1));
+		}
+
+	}
+
+	fclose(fp);
+
+
+
+
+}
