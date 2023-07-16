89c736eaa767e78202be8f084117785a6f47f484
Manvendra Manu <rescue11080@gmail.com>
2023-07-16 15:52:17 +0300

    written hdf5 output command

--- py_utilities/high_N_to_low_N.py
+++ py_utilities/high_N_to_low_N.py
@@ -38,20 +38,27 @@ f_out = "_".join(foutlist)
 
 print(f_in,f_out)
 fp_in = h5py.File(f_in+".hdf5", "r")
-if use_zeldo==1:
-    psi_in = fp_in["psi_zeldo"]
-else:
-    psi_in = fp_in["psi"]
 
+psi_in_zeldo = fp_in["psi_zeldo"]
+
+psi_in = fp_in["psi"]
+
+psi_in_zeldo_t  = np.reshape(psi_in_zeldo,(n_high,n_high,n_high,2))
 psi_in_t  = np.reshape(psi_in,(n_high,n_high,n_high,2))
 
 skp = int(n_high/n_low)
 
+psi_out_zeldo  = psi_in_zeldo_t[::skp,::skp,::skp,:]
 psi_out  = psi_in_t[::skp,::skp,::skp,:]
 
+psi_out_zeldo = np.reshape(psi_out_zeldo,(n_low*n_low*n_low,2))
 psi_out = np.reshape(psi_out,(n_low*n_low*n_low,2))
 
-print(psi_in.shape,psi_out.shape,skp,n_name)
+with h5py.File(f_out+".hdf5", "w") as ff:
+    ff.create_dataset("psi_zeldo",psi_out_zeldo.shape,data=psi_out_zeldo)
+    ff.create_dataset("psi",psi_out.shape,data=psi_out)
+
+print(psi_in.shape,psi_out.shape,psi_in_zeldo.shape,psi_out_zeldo.shape,skp,n_name)
 
 
 
